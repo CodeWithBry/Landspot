b1315f5ab15c4a3dfd604cf492ad492e53fc4f57
CodeWithBry <bryanagustinpajarillaga@gmail.com>
2026-08-19 22:10:31 +0800

    Fixed CORS error policy for URL access.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,10 @@ import MailsRoute from "./routes/mailsRoute";
 
 export const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }))
+app.use(cors({ 
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+}))
 app.use(express.json())
 
 
