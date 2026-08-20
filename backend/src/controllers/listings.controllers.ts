ab9aa88678baa9c23c96e086d3d9d87be01e29fd
CodeWithBry <bryanagustinpajarillaga@gmail.com>
2026-08-20 12:21:09 +0800

    Return listings with is_favorite value.

--- backend/src/controllers/listings.controllers.ts
+++ backend/src/controllers/listings.controllers.ts
@@ -196,7 +196,12 @@ export const loadListingInitially = async (req: Request, res: Response) => {
                         ORDER BY li.display_order
                     ) FILTER (WHERE li.id IS NOT NULL),
                     '[]'
-                ) AS images
+                ) AS images,
+                EXISTS (
+                    SELECT 1 
+                    FROM favorites f 
+                    WHERE f.listing_id = l.id AND f.user_id = $9
+                ) AS is_favorite
             FROM listings l
             LEFT JOIN listing_images li ON li.listing_id = l.id
             GROUP BY l.id
